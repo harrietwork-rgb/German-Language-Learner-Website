d19281bec7e18573f024de9198b9aca9139ac6a9
harrietwork-rgb <harrietworkacc05@gmail.com>
2026-07-08 17:54:46 +0100

    Add showPage function to manage page visibility

---
+++ app.js
@@ -0,0 +1,20 @@
+function showPage(pageId){
+
+
+    const pages =
+    document.querySelectorAll(".page");
+
+
+    pages.forEach(page=>{
+
+        page.classList.remove("active");
+
+    });
+
+
+    document
+    .getElementById(pageId)
+    .classList.add("active");
+
+
+}
